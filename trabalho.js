381ff981114adce2774d8cf66049462602c7ff1b
giovannamt <jojo.maia.tavares@gmail.com>
2025-05-31 17:49:05 -0300

    Entrega do trabalho - implementação dos scripts

---
+++ trabalho.js
@@ -0,0 +1,21 @@
+export function geradorDeTagsDeIdentificacao(nome) {
+  return nome.toUpperCase();
+}
+
+export function verificarSePodeSerAdotado(idade, porte) {
+  return idade === 1 && porte === 'M';
+}
+
+export function calcularConsumoDeRacao(nome, idade, peso) {
+  return peso * 300;
+}
+
+export function decidirTipoDeAtividadePorPorte(porte) {
+  if (porte === 'pequeno') {
+    return 'brincar dentro de casa';
+  }
+}
+
+export async function buscarDadoAsync() {
+  return 'Pipoca';
+}
